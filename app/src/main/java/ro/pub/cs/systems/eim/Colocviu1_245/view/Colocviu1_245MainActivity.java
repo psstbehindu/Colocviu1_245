a0824f05c9309a4fefff6f93ff0fff372f4a1d9d
PsstBehindU <tarqinius@gmail.com>
2020-04-09 13:01:34 +0300

    rezolvareA

---
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/view/Colocviu1_245MainActivity.java
@@ -0,0 +1,16 @@
+package ro.pub.cs.systems.eim.Colocviu1_245.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ro.pub.cs.systems.eim.Colocviu1_245.R;
+
+public class Colocviu1_245MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_245_main);
+    }
+}
